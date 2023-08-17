9a15d74b9afcc4f6753c7fe44f52de1e538fc10e
Felix Martinez Casadiego <felixmartinezcasadiego@MacBook-Pro-de-Felix.local>
2023-08-17 23:02:32 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Home from './src/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
+};
+
+export default App;
